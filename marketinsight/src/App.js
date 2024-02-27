e2a0c9ac9a65310e26a27f9bcde563ba5ca9d5f7
ayush agrawal <ayushagrawal2525@gmail.com>
2024-02-27 17:40:10 +0530

    changes

--- marketinsight/src/App.js
+++ marketinsight/src/App.js
@@ -1,6 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Login from "./component/Login";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB3ywphesVPm0JYv3fmIHTy6Dp7uzCifN4",
+  authDomain: "marketinsight-c6c67.firebaseapp.com",
+  projectId: "marketinsight-c6c67",
+  storageBucket: "marketinsight-c6c67.appspot.com",
+  messagingSenderId: "867196264404",
+  appId: "1:867196264404:web:f83ff1550b82e8be524041",
+  measurementId: "G-HXNKYDC346"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
